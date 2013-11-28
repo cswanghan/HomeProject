96a8a6f3077fa9a987677c0d1344eb60257ef55d
cswanghan <cswanghan@gmail.com>
2013-11-28 22:32:18 +0800

    Change the folder path, and add SampleAdFile

--- HomeProject/src/com/home/DataProcess/ADImpressionSimulator.java
+++ HomeProject/src/com/home/DataProcess/ADImpressionSimulator.java
@@ -1,5 +1,11 @@
 package com.home.DataProcess;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 public class ADImpressionSimulator {
 
     public int[] numbers = new int[] {1,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20}; 
@@ -37,16 +43,44 @@ public class ADImpressionSimulator {
 
 	}
     
+    public void FileReader(String file) throws Exception
+    {
+		String input = file;
+		InputStreamReader InputFile = null;
+		try {
+			InputFile = new InputStreamReader(new FileInputStream(input),"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		BufferedReader FileReader = new BufferedReader(InputFile);
+		
+		String temp = null;
+		while((temp = FileReader.readLine()) != null)
+		{
+			String[] details = temp.split("\t");
+			String ad_id = details[0];
+			String eCPM = details[1];
+			String arrange = details[2];
+			System.out.println(temp);
+		}
+			
+    }
+    
+    
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int key = 14;
+	public static void main(String[] args) throws Exception{
+			// TODO Auto-generated method stub
+	//		int key = 14;
+	//		ADImpressionSimulator bs = new ADImpressionSimulator();
+	//        int low = 0;
+	//        int high = bs.numbers.length;
+	//        bs.performSearch(bs.numbers, key, low, high); 
 		ADImpressionSimulator bs = new ADImpressionSimulator();
-        int low = 0;
-        int high = bs.numbers.length;
-        bs.performSearch(bs.numbers, key, low, high); 
+		bs.FileReader("externalsrc/SampleAdFile");
 	}
 
 	
